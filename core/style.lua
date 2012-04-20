a759fdb0270ddb40ad4b559780917b7d73cfcd4d
Nathan Lilienthal <Nathan@Nathans-MacBook-Pro-2.local>
2012-04-20 03:04:37 -0400

    adding some base structure to the project, nothing actually written yet

---
+++ core/style.lua
@@ -0,0 +1,10 @@
+-----------------------------------------------------------------------
+-- These are the functions that make the frames & other elements of the 
+-- user interface.
+-----------------------------------------------------------------------
+
+-- just jotting this down, doubt it works
+function CreateFrame.size(x, y)
+	CreateFrame.x = x
+	CreateFrame.y = y
+end
