1625a59ea86d519b59a7aef16bbc380669e566b4
Epicgrim <nathanlil13@me.com>
2012-04-25 04:22:36 -0400

    removing old files, and updating XML

--- modules/chat.lua
+++
@@ -1,8 +0,0 @@
-local addon, ns = ...
-local EUI = ns.EUI
------------------------------------------------------------------------
---
------------------------------------------------------------------------
-
-local eui_chat = EUI:CreatePanel("EUI_CHAT_FRAME", UIParent, 300, 175)
-eui_chat:SetPoint("CENTER")
