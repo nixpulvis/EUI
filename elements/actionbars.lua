97a448796914cb955935c4d8c4c22b448a133355
Nathan Lilienthal <Nathan@Nathans-MacBook-Pro-2.local>
2012-04-24 23:36:12 -0400

    api integration

---
+++ elements/actionbars.lua
@@ -0,0 +1,8 @@
+-----------------------------------------------------------------------
+-- EUI Actionbars
+-----------------------------------------------------------------------
+
+-- Disable Stock World of Warcraft Actionbars
+
+local test = CreateFrame("Frame")
+test:Test()
