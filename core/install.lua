76699baf72bff5c464d52b7039d1efc8d3a5156d
Epicgrim <nathanlil13@me.com>
2012-04-26 14:35:33 -0400

    install started, actionbar file made

---
+++ core/install.lua
@@ -0,0 +1,16 @@
+local M, S, V = unpack(select(2, ...))
+-----------------------------------------------------------------------
+-- EUI Install
+-----------------------------------------------------------------------
+
+local function install()
+	print("Installing...")
+end
+
+local f = CreateFrame("Frame")
+f:RegisterEvent("ADDON_LOADED")
+f:SetScript("OnEvent", function(self, name, ...) 
+	if name == "EUI" and not V.saved.installed then
+		install()
+	end
+end)
---
+++ elements/actionbar.lua
@@ -0,0 +1,9 @@
+local M, S, V = unpack(select(2, ...))
+-----------------------------------------------------------------------
+-- EUI Actionbars
+-----------------------------------------------------------------------
+local actionbars = EUI:NewModule("actionbars")
+
+function actionbars:load()
+	
+end
