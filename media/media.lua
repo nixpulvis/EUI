782abc8009c2bf3dbe1413d340393a23a26237e8
Epicgrim <nathanlil13@me.com>
2012-04-28 22:12:37 -0400

    adding media

---
+++ media/media.lua
@@ -0,0 +1,21 @@
+local E, S, V = unpack(select(2, ...))
+-----------------------------------------------------------------------
+-- EUI Media
+-----------------------------------------------------------------------
+
+V.media = {
+	tex = { },
+	icons = { },
+	fonts = { }
+}
+
+local tex = V.media.tex
+local icons = V.media.icons
+local fonts = V.media.fonts
+
+---------- Textures -----------
+tex["blank"] = [[Interface\AddOns\EUI\media\blank]]
+
+------------ Icons -------------
+
+------------ Fonts -------------
