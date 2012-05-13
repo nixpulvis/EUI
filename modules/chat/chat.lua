5f1425c4137ee38208d9dce1c6e3fde3bbe6591e
Epicgrim <nathanlil13@me.com>
2012-05-13 16:34:06 -0600

    refactoring chat.lua

--- modules/chat/chat.lua
+++ modules/chat/chat.lua
@@ -5,89 +5,23 @@ local M, S, V = unpack(select(2, ...))
 local chat = V:NewModule("chat")
 
 function chat:load()		
-	-- show and hide backgrounds
-	hooksecurefunc("FCF_UnDockFrame", function(chatframe)
-		local id = chatframe:GetID()
-		local shown = select(7, FCF_GetChatWindowInfo(chatframe:GetID()))
-
-		if shown and _G["EUIChatFrame"..id] then
-			_G["EUIChatFrame"..id]:Show()
-		end
-	end)
-	hooksecurefunc("FCF_DockFrame", function(chatframe, index, selected)
-		local id = chatframe:GetID()
-		if id == 1 or not _G["EUIChatFrame"..id] then return end
-		_G["EUIChatFrame"..id]:Hide()
-	end)
-	hooksecurefunc("FCF_Close", function(chatframe, index, selected)
-		local id = chatframe:GetID()
-
-		if _G["EUIChatFrame"..id] then
-			_G["EUIChatFrame"..id]:Hide()
-		end
-	end)
-
-	-- Style default windows (1 - 10)
-	for i = 1, NUM_CHAT_WINDOWS do
-		local blizzcf = _G["ChatFrame"..i]
-		local editbox = _G["ChatFrame"..i.."EditBox"]
-		local tab = _G["ChatFrame"..i.."Tab"]
 	
-		-- give every chatframe a background. just hide it until needed.
-		local background = V:CreateElement("EUIChatFrame"..i, chat, UIParent)
-		background:SetFrameStrata("BACKGROUND")
-		background:SetFrameLevel(0)
-		background:SetPoint("TOPLEFT", blizzcf, "TOPLEFT", -4, 28)
-		background:SetPoint("BOTTOMRIGHT", blizzcf, "BOTTOMRIGHT", 2, -6)
-		if  i ~= 1 then background:Hide() end
-	
-		-- remove blizzard style
-		blizzcf:StripTextures()
-		editbox:StripTextures()
-	
-		-- skin editbox
-		editbox:StyleFrame(.9)
-		editbox:ClearAllPoints()
-		editbox:SetPoint("BOTTOMLEFT", background, "BOTTOMLEFT")
-		editbox:SetPoint("BOTTOMRIGHT", background, "BOTTOMRIGHT")
-		editbox:SetHeight(24)
-		
-		-- everytime the editbox would normally lower alpha, hide it
-		editbox:Hide()
-		editbox:HookScript("OnEditFocusLost", function(self) self:Hide() end)
-	
-		-- hide the chat arrow buttons.
-		_G["ChatFrame"..i.."ButtonFrame"]:Hide()
-		_G["ChatFrame"..i.."ButtonFrame"]:SetScript("OnShow", function(self)
-			self:Hide()
-		end)
-		
-		-- tabs
-		tab:StripTextures()
-		-- make that line under each tab
-		tab.bottomline = V:CreateFrame(nil, tab)
-		tab.bottomline:SetPoint("LEFT", tab, "BOTTOMLEFT")
-		tab.bottomline:SetPoint("RIGHT", tab, "BOTTOMRIGHT")
-		tab.bottomline:SetHeight(1)		
-	end
-	
-	-- Style new windows (Whisper/RealID/Convo)
-	hooksecurefunc("FCF_OpenTemporaryWindow", function()
-		local blizzcf = FCF_GetCurrentChatFrame()
-		local name = blizzcf:GetName()
+	-- skins the given chatframe
+	local function SkinChatFrame(chatframe)
+		local name = chatframe:GetName()
 		local editbox = _G[name.."EditBox"]
 		local tab = _G[name.."Tab"]
 	
 		-- give every chatframe a background. just hide it until needed.
-		local background = V:CreateElement("EUI".. name, chat, UIParent)
+		local background = V:CreateElement("EUI"..name, chat, UIParent)
 		background:SetFrameStrata("BACKGROUND")
 		background:SetFrameLevel(0)
-		background:SetPoint("TOPLEFT", blizzcf, "TOPLEFT", -4, 28)
-		background:SetPoint("BOTTOMRIGHT", blizzcf, "BOTTOMRIGHT", 2, -6)
-		background:Hide()
+		background:SetPoint("TOPLEFT", chatframe, "TOPLEFT", -4, 28)
+		background:SetPoint("BOTTOMRIGHT", chatframe, "BOTTOMRIGHT", 2, -6)
+		if not name == "ChatFrame1" then background:Hide() end
 		
 		-- remove blizzard style
-		blizzcf:StripTextures()
+		chatframe:StripTextures()
 		editbox:StripTextures()
 	
 		-- skin editbox
@@ -96,6 +30,21 @@ function chat:load()
 		editbox:SetPoint("BOTTOMLEFT", background, "BOTTOMLEFT")
 		editbox:SetPoint("BOTTOMRIGHT", background, "BOTTOMRIGHT")
 		editbox:SetHeight(24)
+		
+		-- update border color based on active chat
+		hooksecurefunc("ChatEdit_UpdateHeader", function()
+			local type = editbox:GetAttribute("chatType")
+			if ( type == "CHANNEL" ) then
+				local id = GetChannelName(editbox:GetAttribute("channelTarget"))
+				if id == 0 then
+					editbox:SetBackdropBorderColor(unpack(C.media.bordercolor))
+				else
+					editbox:SetBackdropBorderColor(ChatTypeInfo[type..id].r,ChatTypeInfo[type..id].g,ChatTypeInfo[type..id].b)
+				end
+			else
+				editbox:SetBackdropBorderColor(ChatTypeInfo[type].r,ChatTypeInfo[type].g,ChatTypeInfo[type].b)
+			end
+		end)
 		
 		-- everytime the editbox would normally lower alpha, hide it
 		editbox:Hide()
@@ -113,7 +62,45 @@ function chat:load()
 		tab.bottomline = V:CreateFrame(nil, tab)
 		tab.bottomline:SetPoint("LEFT", tab, "BOTTOMLEFT")
 		tab.bottomline:SetPoint("RIGHT", tab, "BOTTOMRIGHT")
-		tab.bottomline:SetHeight(1)	
+		tab.bottomline:SetHeight(1)
+	end
+	
+	-- skin the default 10 chat friends
+	for i = 1, NUM_CHAT_WINDOWS do
+		local chatframe = _G["ChatFrame"..i]
+		SkinChatFrame(chatframe)
+	end
+	
+	-- Style new windows (Whisper/RealID/Convo)
+	hooksecurefunc("FCF_OpenTemporaryWindow", function()
+		local chatframe = FCF_GetCurrentChatFrame()
+		SkinChatFrame(chatframe)
+	end)
+	
+	-- show background on UnDocking
+	hooksecurefunc("FCF_UnDockFrame", function(chatframe)
+		local id = chatframe:GetID()
+		local shown = select(7, FCF_GetChatWindowInfo(chatframe:GetID()))
+
+		if shown and _G["EUIChatFrame"..id] then
+			_G["EUIChatFrame"..id]:Show()
+		end
+	end)
+	
+	-- hide background on Docking	
+	hooksecurefunc("FCF_DockFrame", function(chatframe, index, selected)
+		local id = chatframe:GetID()
+		if id == 1 or not _G["EUIChatFrame"..id] then return end
+		_G["EUIChatFrame"..id]:Hide()
+	end)
+	
+	-- hide background on closing
+	hooksecurefunc("FCF_Close", function(chatframe, index, selected)
+		local id = chatframe:GetID()
+
+		if _G["EUIChatFrame"..id] then
+			_G["EUIChatFrame"..id]:Hide()
+		end
 	end)
 	
 	ChatFrameMenuButton:Kill()
@@ -123,7 +110,6 @@ function chat:load()
 	hooksecurefunc("ChatEdit_SetLastActiveWindow", function(editbox)
 		editbox:SetAlpha(0)
 	end)
-	
 	
 	--add Friends and Guild pane
 	V.CreateFriendsPanel()
