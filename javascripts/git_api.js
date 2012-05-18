da58331fc812aa16158ebe2942ed2a2b9f933c9f
Epicgrim <nathanlil13@me.com>
2012-05-18 17:07:42 -0600

    dynamic commit and aouthor info

---
+++ javascripts/git_api.js
@@ -0,0 +1,35 @@
+// not abstracted for other repos
+function git_GET_collaborators() {
+	$.ajax({
+	  url: "https://api.github.com/repos/Epicgrim/EUI/collaborators",
+	}).done(function(data) {
+		$(".developers").empty();
+		for(var i=0; i < data.length; i++){
+			var name = data[i]["login"];
+			
+			$(".developers").append("<li><a href=https://github.com/"+name+">"+name+"</a></li>");
+		}
+	});
+}
+
+// not abstracted for other repos
+function git_GET_lastestCommit() {
+	$.ajax({
+	  url: "https://api.github.com/repos/Epicgrim/EUI/commits",
+	}).done(function(data) {
+		$(".commits").empty();
+		for(var i=0; i < 5; i++){
+			var msg = data[i]["commit"]["message"];
+			var api_url = data[i]["url"];
+			var url = api_url.split("/").pop();
+			
+			$(".commits").append("<li><a href=https://github.com/Epicgrim/EUI/commit/"+url+">"+msg+"</a></li>");
+		}
+	});
+}
+
+//handle updating commits
+$(document).ready(function() {
+	git_GET_collaborators();
+	git_GET_lastestCommit();
+});
