cdbccfde2032b40fa1ef792213e67df7eefd19d7
Epicgrim <nathanlil13@me.com>
2012-05-22 09:42:00 -0600

    jsonp??

--- javascripts/git_api.js
+++ javascripts/git_api.js
@@ -3,8 +3,9 @@ function git_GET_collaborators() {
 	$.ajax({
 	  	url: "https://api.github.com/repos/Epicgrim/EUI/collaborators",
 	 	crossDomain: true,
-	    dataType: 'json'	
-	}).done(function(data) {
+	    dataType: 'jsonp'	
+	}).done(function(response) {
+		var data = response["data"];
 		$(".developers").empty();
 		for(var i=0; i < data.length; i++){
 			var name = data[i]["login"];
@@ -19,8 +20,9 @@ function git_GET_lastestCommit() {
 	$.ajax({
 	  	url: "https://api.github.com/repos/Epicgrim/EUI/commits",
 	 	crossDomain: true,
-	    dataType: 'json'
-	}).done(function(data) {
+	    dataType: 'jsonp'
+	}).done(function(response) {
+		var data = response["data"];
 		$(".commits").empty();
 		for(var i=0; i < 5; i++){
 			var msg = data[i]["commit"]["message"];
