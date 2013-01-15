af40b7de744c293eae0c4b971e0f078989505b9c
Nathan Lilienthal <nathanlil13@me.com>
2013-01-15 00:12:21 -0500

    check this in with TEST off.
    tests are really just for me.

--- init.lua
+++ init.lua
@@ -15,6 +15,6 @@ ns[3] = { }
 ns[4] = { }
 
 ns[3].DEBUG = true -- toggle EUI debugging, prints extra details in some cases
-ns[3].TEST  = true -- toggle EUI's testing, located in dev/tests.lua
+ns[3].TEST = false -- toggle EUI's testing, located in dev/tests.lua
 
 EUI = ... -- expose ourselves (dirty I know)
