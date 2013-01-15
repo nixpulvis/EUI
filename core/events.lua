45abad4c99c1bc921cb1d547468d56344237406f
Nathan Lilienthal <nathanlil13@me.com>
2013-01-14 19:18:37 -0500

    untested, but idea driven event code

--- core/events.lua
+++ core/events.lua
@@ -3,28 +3,45 @@ local M, S, V, F = unpack(select(2, ...))
 -- EUI EventMachine
 -----------------------------------------------------------------------
 --[[ this is designed to make updating information possible without the
-   need to make a frame just for event handling. ]]
+   need to make a frame just for event handling.
+   
+   Example Usage:
+     F.EventMachine:add("PLAYER_LEVEL_UP", function()
+       print("You leveled up")
+     end)
+     
+     As you can see this is useful for doing things on events
+     that are not associated with a perticular frame. ]]
 
 F.EventMachine = {
   ['events']       = {},
   ['eventhandler'] = CreateFrame("Frame"),
 }
 
--- adds the given function to the given event
-function F.EventMachine:add(event, func)
+-- F.EventMachine:add : String, Function -> Number
+-- Returns the index of this function on a per event basis.
+-- adds the given function to the given event. see :remove,
+-- for information on removing functions from events.
+function F.EventMachine:add(event, func).
   if self.events[event] == nil then
-    self.events[event] = { }
+    self.events[event] = {}
   end
   tinsert(self.events[event], func)
   self.eventhandler:RegisterEvent(event)
 end
 
--- remove the given function from the given event
-function F.EventMachine:remove(event, func)
-  for i,v in ipairs(self.events[event]) do
-    if v == func then tremove(self.events[event], i) end
+-- F.EventMachine:remove : String, Number -> Boolean
+-- Returns true if the given event had a function at the
+-- given index, and it was removed from the event machine.
+-- The index is the number returned from :add.
+function F.EventMachine:remove(event, index)
+  --for i,v in ipairs(self.events[event]) do
+  --  if v == func then tremove(self.events[event], i) end
+  --end
+  if self.events[index] then self.events[index] = nil end
+  -- if the event doesn't have any functions unregister it.
+  if #self.events[event] == 0 then self.eventhandler:UnregisterEvent(event)
   end
-  if #self.events[event] == 0 then self.eventhandler:UnregisterEvent(event) end
 end
 
 -- start up the event machine, by binding it's OnEvent script
