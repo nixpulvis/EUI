00fdcbfdb7d865d77aba114ea33cc11255b786f4
Nathan Lilienthal <nathan@nixpulvis.com>
2013-12-16 00:11:13 -0500

    Functions to spec.

--- core/functions.lua
+++ core/functions.lua
@@ -1,27 +1,30 @@
-local M, S, V, F = unpack(select(2, ...))
+local E, S, M = unpack(select(2, ...))
 -----------------------------------------------------------------------
 -- EUI Functions
 -----------------------------------------------------------------------
---[[ these are EUI's utilities, they provide somewhat abstracted
-   functionality. All functions here are stored in F ]]
+--[[ These are EUI's utilities, they provide somewhat abstracted
+     functionality. ]]
 
--- shallow table copy.
-function F.tcopy(t)
-  local copy = { }
+-- E:TableCopy : table -> table
+-- Return a shallow copy of the given table.
+function E:TableCopy(t)
+  local copy = {}
   for k,v in pairs(t) do
     copy[k] = v
   end
   return copy
 end
 
--- rounding numbers
-function F.round(n, decimals)
+-- E:Round : number, number -> number
+-- Rounding numbers, because sometimes 10.2135213 isn't helpful.
+function E:Round(n, decimals)
   local mult = 10^(decimals or 0)
   return math.floor(n * mult + 0.5) / mult
 end
 
--- converts hex colors to 0-1 colors
-function F.hextocolor(hex)
+-- E:HexToColor : string -> table
+-- Converts hex colors to 0-1 colors.
+function E.HexToColor(hex)
   local color = { }
   if strlen(hex) == 6 or strlength(hex) == 8 then
     for i = 1, 8, 2 do
@@ -35,8 +38,9 @@ function F.hextocolor(hex)
   return unpack(color)
 end
 
--- display seconds to min/hour/day (tukui <3)
-function F.formattime(string)
+-- E:FormatTime : string -> string
+-- Display seconds to min/hour/day.
+function E.FormatTime(string)
   local day, hour, minute = 86400, 3600, 60
   if string >= day then
     return format("%dd", ceil(string / day))
@@ -50,43 +54,45 @@ function F.formattime(string)
   return '|cFFFF2222'..format("%.1f", string)..'|r'
 end
 
--- merge the metatable of the given frame with the given metatable
-function F.extend(frame, meta)
-  setmetatable(V.TMerge(getmetatable(frame), meta))
-end
+-- TODO
 
--- a timer, with callbacks
-F.Timer = { }
-function F.Timer.create(precision)
-  local self     = F.tcopy(F.Timer)
-  self.precision = (precision or 2)
+-- -- merge the metatable of the given frame with the given metatable
+-- function F.extend(frame, meta)
+--   setmetatable(V.TMerge(getmetatable(frame), meta))
+-- end
 
-  -- the update frame
-  self.engine = CreateFrame('Frame')
+-- -- a timer, with callbacks
+-- F.Timer = { }
+-- function F.Timer.create(precision)
+--   local self     = F.tcopy(F.Timer)
+--   self.precision = (precision or 2)
 
-  return self
-end
-function F.Timer:remove()
-  self.engine:kill()
-  self = nil
-end
-function F.Timer:start( time_left, post_update, post_complete )
-  self.time_initial  = time_left
-  self.time_left     = time_left
-  self.post_update   = post_update
-  self.post_complete = post_complete
+--   -- the update frame
+--   self.engine = CreateFrame('Frame')
 
-  self.engine:SetScript('OnUpdate', function(engine, elapsed)
-    if self.time_left <= elapsed then
-      if post_update then self.post_update(0) end
-      if post_complete then self.post_complete() end
-      self.engine:SetScript('OnUpdate', nil)
-    else
-      if self.post_update then self.post_update(V.round(self.time_left, self.precision)) end
-    end
-    self.time_left = self.time_left - elapsed
-  end)
-end
-function F.Timer:reset()
-  self.time_left = self.time_initial
-end
+--   return self
+-- end
+-- function F.Timer:remove()
+--   self.engine:kill()
+--   self = nil
+-- end
+-- function F.Timer:start( time_left, post_update, post_complete )
+--   self.time_initial  = time_left
+--   self.time_left     = time_left
+--   self.post_update   = post_update
+--   self.post_complete = post_complete
+
+--   self.engine:SetScript('OnUpdate', function(engine, elapsed)
+--     if self.time_left <= elapsed then
+--       if post_update then self.post_update(0) end
+--       if post_complete then self.post_complete() end
+--       self.engine:SetScript('OnUpdate', nil)
+--     else
+--       if self.post_update then self.post_update(V.round(self.time_left, self.precision)) end
+--     end
+--     self.time_left = self.time_left - elapsed
+--   end)
+-- end
+-- function F.Timer:reset()
+--   self.time_left = self.time_initial
+-- end
